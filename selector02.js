e960e2456a6b4274e44607ab6bef6548b838cfd4
potatissallad <a.hoagland12439@gmail.com>
2016-09-23 08:17:14 -0600

    Add files via upload

---
+++ selector02.js
@@ -0,0 +1,14 @@
+$(document).ready(function()
+
+	$("#butonpic").hide();
+	$("#testbutton").click(function() {
+		
+		$("#buttonpic").slidetoggle(500);
+		/*
+		$("#selectors02").css("background-color","red");
+		$("h3, #testbutton").css("background-color", "red");
+		$("*").css("background-color", "red");
+		*/
+	});
+
+});
